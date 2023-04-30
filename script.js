b3df50287d38df104f40cc546065e603892f70aa
AlexBoronin <acivinel@mail.ru>
2023-05-01 00:00:34 +0300

    feat: add array off key items

--- script.js
+++ script.js
@@ -19,19 +19,48 @@
     wrapContent.append(keyboard);
 
 
-    function addRow(){
+    function addRow() {
         let n = 0;
+        let tmp
         const keyRow = document.createElement('div');
-        while (n !== 5){
+        while (n !== 5) {
             keyRow.setAttribute('style', 'display: flex; justify-content: space-between; align-items: center; height: 10px; background-color: rgb(59, 35, 35); margin-bottom: 10px; box-sizing: border-box');
             keyRow.setAttribute('id', `row${n}`);
-            if (n === 4){
+            if (n === 4) {
                 keyRow.style.marginBottom = '0';
             }
             keyboard.append(keyRow);
-            keyboard.innerHTML = keyboard.innerHTML;
+            tmp = keyboard.innerHTML;
+            keyboard.innerHTML = tmp;
             n++
         }
     }
+
     addRow();
+
+    const keysRowFirst = [
+        {one: ['\`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace']},
+        {sub: ['~', '!', '@', '#', '$', '%', ':', '?', '*', '(', ')', '_', '+', '']}
+    ];
+    const keysRowSecond = [
+        {one: ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'DEL']},
+        {sub: ['', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '/', '']}
+    ];
+    const keysRowThird = [
+        {one: ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'ENTER']},
+        {sub: ['', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', '']}
+    ];
+    const keysRowFourth = [
+        {one: ['Shift', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'L', ',', '.', '/', ``, 'Shift']},
+        {sub: ['', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '/', '']}
+    ];
+    const keysRowFifth = [{one: ['Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', 'B', 'N', 'M']}];
+
+    console.log(keysRowFirst[0].one[0]);
+    console.log(keysRowSecond[0].one[keysRowSecond[0].one.length - 2]);
+    console.log(keysRowFourth[0].one[keysRowSecond[0].one.length - 2]);
+
+    function addKey() {
+
+    }
 })();
